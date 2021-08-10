22bfd5436bc16531a90980274fc048cbf01e3417
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-10 12:14:01 -0600

    cars table created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,10 +1,10 @@
 exports.up = function (knex) {
-  return knex.shcema.createTable('cars', tbl => {
+  return knex.schema.createTable('cars', tbl => {
     tbl.increments();
     tbl.string('vin').notNullable().unique();
     tbl.string('make').notNullable();
     tbl.string('model').notNullable();
-    tbl.numeric('mileage').notNullable();
+    tbl.integer('mileage').notNullable();
     tbl.string('title');
     tbl.string('transmission');
   })
