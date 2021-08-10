1f89be65bdbf3af9a04123103742334219aaaaec
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-10 11:30:23 -0600

    setting up server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router');
 
-const server = express()
+
+const server = express();
+
+server.use(express.json());
 
 // DO YOUR MAGIC
 
+
+server.use('*', (req, res) => {
+    res.status(404).json({message: 'not fount'})
+})
 module.exports = server
