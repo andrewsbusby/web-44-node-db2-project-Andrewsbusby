88472d7c464630f1338532f05ac01a4b7497c5c6
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-10 13:10:20 -0600

    setting up cars router and connecting to server

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    res.json('get all cars')
+})
+
+router.get('/:id', async (req, res, next) => {
+    res.json('get car by id')
+})
+
+router.post('/', async (req, res, next) => {
+    res.json('creating new car')
+})
+
+module.exports = router;
