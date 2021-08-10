a2964beac48da709a4b5bed342d8b4f28efc8caf
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-10 12:33:20 -0600

    setting up seed for cars table

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
-// STRETCH
+const cars = [
+    {
+        vin: '3GNFK16318G269795',
+        make: 'toyota',
+        model: 'camrey',
+        mileage: 100000,
+    },
+    {
+        vin: '1HD1FMW166Y641723',
+        make: 'ford',
+        model: 'focus',
+        mileage: 150000,
+        transmission: 'manual'
+    },
+    {
+        vin: 'JH4KA3261JC024072',
+        make: 'handa', 
+        model: 'civic',
+        mileage: 50000,
+        title: 'clean',
+        transmission: 'auto'
+    }
+]
+
+exports.seed = function(knex) {
+    return knex('cars').truncate()
+    .then(()=> {
+        return knex('cars').insert(cars)
+    })
+}
