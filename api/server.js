efcea25b4cced4deddbb6dadf88ce45042ba411f
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-10 12:54:44 -0600

    setting up error handling

--- api/server.js
+++ api/server.js
@@ -9,7 +9,14 @@ server.use(express.json());
 // DO YOUR MAGIC
 
 
-server.use('*', (req, res) => {
-    res.status(404).json({message: 'not fount'})
+server.use('*', (req, res, next) => {
+    next({ status: 404, message: 'not found'})
 })
+
+server.use((err, req, res, next) => {//eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
 module.exports = server
