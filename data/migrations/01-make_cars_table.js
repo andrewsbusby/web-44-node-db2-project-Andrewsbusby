9fc95d709b88fdacdb3f88427a7cac0d6ed8b610
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-10 12:37:21 -0600

    added what length for vin

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', tbl => {
     tbl.increments();
-    tbl.string('vin').notNullable().unique();
+    tbl.string('vin', 17).notNullable().unique();
     tbl.string('make').notNullable();
     tbl.string('model').notNullable();
     tbl.integer('mileage').notNullable();
